283e219eb25946653b1ed71732f6bf4fa24f1c12
Ivan Chiang <ivan21916@gmail.com>
2020-04-14 00:41:34 -0700

    add for loop for printing products

--- OneDrive/Desktop/products/products.py
+++ OneDrive/Desktop/products/products.py
@@ -3,6 +3,9 @@ while True:
 	name = input('Please enter the name of product: ')
 	if name == 'q':
 		break
-	price = input('Please enter the price of product: ')
+	price = input('Please enter the price of product (dollar): ')
 	products.append([name, price])
 print(products)
+
+for p in products:
+	print('The price of', p[0], 'is', p[1], 'dollars')
